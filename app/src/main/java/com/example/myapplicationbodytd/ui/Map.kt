5737ace7f2970078e302d8f060e9c295b04a4a1d
igeorges1 <iliamo@hotmail.fr>
2025-04-10 22:26:50 +0200

    basic template

--- app/src/main/java/com/example/myapplicationbodytd/ui/Map.kt
+++ app/src/main/java/com/example/myapplicationbodytd/ui/Map.kt
@@ -1,14 +1,1 @@
-//package com.example.myapplicationbodytd.ui
-//
-//data class Position(val x: Int, val y: Int)
-//
-//class GameMap {
-//    fun isPlacementValid(pos: Position): Boolean = true
-//    fun getPathPositions(): List<Position> = listOf()
-//    fun placeTurret(turret: Turret, position: Position): Boolean {
-//        return if (isPlacementValid(position)) {
-//            // place turret logic
-//            true
-//        } else false
-//    }
-//}
+package com.example.myapplicationbodytd.ui
